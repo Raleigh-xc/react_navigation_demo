f8e2ed35bdde6f5007dd8d25d6cc224fb7344777
Dorange <974391165@qq.com>
2019-12-02 17:56:58 +0800

    使用redux

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/views/Page08';
+import App from './src/views/Page21';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
